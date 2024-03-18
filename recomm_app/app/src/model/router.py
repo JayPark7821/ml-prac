9967444a738ca4a8fd6d71999cd3c05ae2242014
jaypark <pjhyun1990@gmail.com>
2024-03-18 22:14:50 +0900

    recomm app

--- recomm_app/app/src/model/router.py
+++ recomm_app/app/src/model/router.py
@@ -1,6 +1,7 @@
 import torch
 from fastapi import APIRouter
 
+from recomm_app.app.src.model.model import model
 from recomm_app.app.src.model.schemas import RatingRequest
 from recomm_app.app.src.model.schemas import RatingResponse
 
@@ -9,7 +10,12 @@ router = APIRouter()
 
 @router.post("/predict/", response_model=RatingResponse)
 async def predict_rating(request: RatingRequest) -> RatingResponse:
-
+    user_movie_tensor = torch.tensor(
+        [[request.userId - 1, request.movieId - 1]], dtype=torch.int32
+    )
+    with torch.no_grad():
+        predicted_rating = model(user_movie_tensor).item() * 5
+    rounded_rating = round(predicted_rating * 2) / 2
     return RatingResponse(
-        userId=request.userId, movieId=request.movieId, predictedRating=0.0
+        userId=request.userId, movieId=request.movieId, predictedRating=rounded_rating
     )
